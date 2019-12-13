02eb9ad5853f9fe535c34abe34208299bd4204fc
nak-244 <t09024656860@gmail.com>
2019-12-13 17:42:00 +0900

    up

--- docs/js/search.js
+++ docs/js/search.js
@@ -3,6 +3,6 @@ $(function() {
   $('#srh_area_param,#sfw').on('change', function() {
     var srh_area_param = $('#srh_area_param').val();
     var sfw = $('#sfw').val();
-    $('#test').prop('href', href + "?fuseaction=job.joblist&srh_area_param=5" + "&srh_area_param=" + srh_area_param + "&sfw=" + sfw);
+    $('#test').prop('href', href + "?fuseaction=job.joblist" + "&srh_area_param=" + srh_area_param + "&sfw=" + sfw);
   });
 });
